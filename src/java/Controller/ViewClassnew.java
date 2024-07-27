9aa83349bb003b0493c07a61dd1a15102f47a6d2
TrangTNHHE189002 <trangtran.170204@gmail.com>
2024-07-27 07:01:21 +0700

    trang do class card

---
+++ src/java/Controller/ViewClassnew.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller;
+
+import DAO.AClassDAO;
+import DAO.SessionDAO;
+import Model.AClass;
+import Model.Session;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+/**
+ *
+ * @author TRANG
+ */
+@WebServlet(name="ViewClassnew", urlPatterns={"/ViewClassnew"})
+public class ViewClassnew extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+            AClassDAO aClassDAO = new AClassDAO();
+            SessionDAO sessionDAO = new SessionDAO();
+
+            Vector<AClass> classes = aClassDAO.getClassesByLearnerId(1);
+
+
+            Map<Integer, Session> sessionData = new HashMap<>();
+            for (AClass aClass : classes) {
+                Session classSession = sessionDAO.getSessionByClassId(aClass.getId());
+                sessionData.put(aClass.getId(), classSession);
+            }
+
+            Map<Integer, Double> progressMap = new HashMap<>();
+            for (AClass aClass : classes) {
+                int finishedSessions = aClassDAO.getFinishedSessions(aClass.getId());
+                double progressPercentage = (double) finishedSessions / aClass.getTotalSession() * 100;
+                progressMap.put(aClass.getId(), progressPercentage);
+            }
+
+            request.setAttribute("classes", classes);
+            request.setAttribute("sessionData", sessionData);
+           
+            request.setAttribute("fn", aClassDAO.countClassByStatusLearner("finished", 1));
+            request.setAttribute("og", aClassDAO.countClassByStatusLearner("ongoing", 1));
+            request.getRequestDispatcher("View/ViewClassnew.jsp").forward(request, response);
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
